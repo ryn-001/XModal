7c7b41b6e508093c1b05c7b417830d8ba31c1a82
Aryan Khaire <aryan.khaire001@gmail.com>
2025-11-05 22:31:19 +0530

    Hello World

---
+++ dictionary/src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+
+  [
+
+    { word: "React", meaning: "A JavaScript library for building user interfaces." },
+
+    { word: "Component", meaning: "A reusable building block in React." },
+
+    { word: "State", meaning: "An object that stores data for a component." }
+
+  ]
+
+
+  return (
+    <div className="App">
+
+    </div>
+  );
+}
+
+export default App;
