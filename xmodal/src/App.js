b340fd132467eb82b4911db4ca4e888e5abe4e2c
Aryan Khaire <aryan.khaire001@gmail.com>
2025-11-08 22:15:51 +0530

    Minor test bugs

--- xmodal/src/App.js
+++ xmodal/src/App.js
@@ -2,7 +2,6 @@ import * as React from 'react';
 import Backdrop from '@mui/material/Backdrop';
 import Box from '@mui/material/Box';
 import Modal from '@mui/material/Modal';
-import Fade from '@mui/material/Fade';
 import Button from '@mui/material/Button';
 import { useState } from 'react';
 import "./App.css"
